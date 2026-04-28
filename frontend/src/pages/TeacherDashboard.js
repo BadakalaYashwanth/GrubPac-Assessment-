0b6898aa5ecb3a6efa0b6fb8797980480cd32d51
BadakalaYashwanth <170221536+BadakalaYashwanth@users.noreply.github.com>
2026-04-28 15:41:17 +0530

    Fix build error: remove unused useEffect import in TeacherDashboard

--- frontend/src/pages/TeacherDashboard.js
+++ frontend/src/pages/TeacherDashboard.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { handleError, handleSuccess } from '../utils';
 
 function TeacherDashboard({ contents, fetchContent }) {
